eb25778756f3a3fbae4ab7bb7b9f63346b2ad4a5
Colton <czpemberton@gmail.com>
2019-07-02 09:49:43 -0700

    Mobile Updates

--- main.js
+++ main.js
@@ -1,12 +1,27 @@
-const desc1 = document.querySelector(".desc1");
-const but1 = document.querySelector(".but1");
-const desc2 = document.querySelector(".desc2");
-const but2 = document.querySelector(".but2");
+const toggleMenu = document.querySelector('.toggleMenu');
+const ul = document.querySelector('UL');
+const arrow = toggleMenu.querySelector('SVG');
+const desc1 = document.querySelector('.desc1');
+const but1 = document.querySelector('.but1');
+const desc2 = document.querySelector('.desc2');
+const but2 = document.querySelector('.but2');
 
+let showingUl = false;
 let showingDesc1 = false;
 let showingDesc2 = false;
 
 document.addEventListener('DOMContentLoaded', function () {
+  toggleMenu.addEventListener('click', (e) => {
+    if (showingUl === false) {
+      arrow.style.transform = 'rotate(-180deg)';
+      ul.style.maxHeight = '100vh';
+      showingUl = true;
+    } else {
+      arrow.style.transform = 'rotate(0deg)';
+      ul.style.maxHeight = '0px';
+      showingUl = false;
+    }
+  })
   but1.addEventListener('click', (e) => {
     if (showingDesc1 === false) {
       but1.innerHTML = 'Show less';
